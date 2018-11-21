dfe7a2eda38182276b1628c3c956ea60decbc090
msyksphinz <msyksphinz_dev.gmail.com>
2018-11-21 19:52:02 +0900

    Func: implement bit-width memory

--- src/main/scala/cpu/memory.scala
+++ src/main/scala/cpu/memory.scala
@@ -16,38 +16,47 @@ class MemoryIo (bus_width: Int) extends Bundle {
 class Memory (bus_width: Int) extends Module {
   val io = IO(new MemoryIo(bus_width))
 
-  val memory = Mem(math.pow(2, bus_width).toInt , UInt(64.W))
+  val inst_rd_data = Wire(Vec(8, UInt(8.W)))
+  val data_rd_data = Wire(Vec(8, UInt(8.W)))
 
-  when (io.ext_bus.req) {
-    val tmp_val = memory(io.ext_bus.addr(bus_width-1, 1))
-    when(io.ext_bus.addr(0) === 0.U) {
-      memory(io.ext_bus.addr(bus_width-1, 1)) := Cat(tmp_val(63,32), io.ext_bus.data.asUInt)
-    } .otherwise {
-      memory(io.ext_bus.addr(bus_width-1, 1)) := Cat(io.ext_bus.data.asUInt, tmp_val(31, 0))
+  for (bank <- 0 until 8) {
+
+    val memory = Mem(math.pow(2, bus_width).toInt , UInt(8.W))
+
+    val bank_idx = bank.U(3.W)
+    val data_msb = (bank & 0x3)*8+7
+    val data_lsb = (bank & 0x3)*8+0
+
+    when (io.ext_bus.req) {
+      when(io.ext_bus.addr(0) === bank_idx(2)) {
+        memory(io.ext_bus.addr(bus_width-1, 1)) := io.ext_bus.data(data_msb, data_lsb)
+        printf(p"<Info : Bank ${Decimal(bank_idx)} : Address 0x${Hexadecimal(io.ext_bus.addr)} : Write 0x${Hexadecimal(io.ext_bus.data(data_msb, data_lsb))}>\n")
+      }
     }
-    printf(p"<Info : Address 0x${Hexadecimal(io.ext_bus.addr)} : Write 0x${Hexadecimal(io.ext_bus.data)}>\n")
+
+    /* Inst Bus */
+    inst_rd_data(bank) := memory(io.inst_bus.addr(bus_width-1, 3))
+
+    /* Data Bus */
+    data_rd_data(bank) := memory(io.data_bus.addr(bus_width-1, 3))
+
+    when(io.data_bus.req & (io.data_bus.cmd === CMD_WR) & io.data_bus.addr(2) === bank_idx(2)) {
+      memory(io.data_bus.addr(bus_width-1, 3)) := io.data_bus.wrdata(data_msb, data_lsb)
+    }
   }
 
-  /* Inst Bus */
-  val inst_rd_tmp_val = memory(io.inst_bus.addr(bus_width-1, 3))
-  when(io.inst_bus.addr(2)) {
-    io.inst_bus.rddata := inst_rd_tmp_val(63,32).asSInt
+  io.inst_bus.ack := io.inst_bus.req
+  when(io.inst_bus.req & io.inst_bus.addr(2) === 0.U(1.W)) {
+    io.inst_bus.rddata := Cat(inst_rd_data(3), inst_rd_data(2), inst_rd_data(1), inst_rd_data(0)).asSInt
   } .otherwise {
-    io.inst_bus.rddata := inst_rd_tmp_val(31, 0).asSInt
+    io.inst_bus.rddata := Cat(inst_rd_data(7), inst_rd_data(6), inst_rd_data(5), inst_rd_data(4)).asSInt
   }
-  io.inst_bus.ack    := io.inst_bus.req
 
-  /* Data Bus */
-  val data_rd_tmp_val = memory(io.data_bus.addr(bus_width-1, 3))
-  when(io.inst_bus.addr(2)) {
-    io.data_bus.rddata := data_rd_tmp_val(63,32).asSInt
+  io.data_bus.ack := io.data_bus.req & (io.data_bus.cmd === CMD_RD)
+  when(io.data_bus.req & io.data_bus.addr(2) === 0.U(1.W)) {
+    io.data_bus.rddata := Cat(data_rd_data(3), data_rd_data(2), data_rd_data(1), data_rd_data(0)).asSInt
   } .otherwise {
-    io.data_bus.rddata := data_rd_tmp_val(31, 0).asSInt
-  }
-  io.data_bus.ack    := io.data_bus.req & (io.data_bus.cmd === CMD_RD)
-
-  when(io.data_bus.req & (io.data_bus.cmd === CMD_WR)) {
-    memory(io.data_bus.addr(bus_width-1, 2)) := io.data_bus.wrdata.asUInt
+    io.data_bus.rddata := Cat(data_rd_data(7), data_rd_data(6), data_rd_data(5), data_rd_data(4)).asSInt
   }
 
 }
