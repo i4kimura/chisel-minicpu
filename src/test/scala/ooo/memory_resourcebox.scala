e10f0c8018fdccee6bcfcbc70fed4186982b42ee
msyksphinz <msyksphinz_dev@gmail.com>
2019-08-05 00:56:04 +0900

    Func: update resourceBox

--- src/test/scala/ooo/memory_resourcebox.scala
+++ src/test/scala/ooo/memory_resourcebox.scala
@@ -5,17 +5,17 @@ import chisel3.util._
 import chisel3.Bool
 
 
-
-
 class MemoryResourceBox [Conf <: RVConfig](conf: Conf) extends Module {
   val io = IO(new Bundle {
     val mem = new MemoryIo(conf)
+    val ext = Flipped(new ExtBus(conf))
   })
 
   val mem_resource_box_core = Module(new MemoryResourceBoxCore(conf))
 
   mem_resource_box_core.io.clock <> clock
   mem_resource_box_core.io.mem <> io.mem
+  mem_resource_box_core.io.ext <> io.ext
 }
 
 
@@ -23,6 +23,7 @@ class MemoryResourceBoxCore [Conf <: RVConfig](conf: Conf) extends BlackBox with
   val io = IO(new Bundle {
     val clock = Input(Clock())
     val mem = new MemoryIo(conf)
+    val ext = Flipped(new ExtBus(conf))
   })
 
   setResource ("/memory_real.v")
